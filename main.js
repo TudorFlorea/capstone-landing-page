6f920d54bddc9912d9da9aebfc07786ac1092234
Tudor <ftudorel92@yahoo.com>
2018-11-23 12:04:09 +0200

    Scolling events and final touches

--- main.js
+++ main.js
@@ -3,8 +3,15 @@ $(function() {
   var contactWrapper = $("#contact-wrapper");
   var burgerIcon = $(".burger");
   var dotsIcon = $(".dots");
+  var navWrapper = $('#nav-wrapper');
+  var navList = $('.nav--list');
+  var navSearchIcon = $('#nav-search');
+  var navSearchInput = $('#search-input');
+  var navSearchLabel = $('#search-input-label');
+  var scrollTop = $("#scoll-top");
   var isMobileMenuOpen = false;
   var isMobileContactOpen = false;
+  var isDesktopSearchActive = false;
 
   $(".nav--link").on("click", function(e) {
     if(isMobileNav()) {
@@ -24,6 +31,28 @@ $(function() {
   $('#nav, header').on('click', function(e) {
     e.stopPropagation();
   });
+  
+  navSearchIcon.on('click', function(e) {
+    e.preventDefault();
+    if(!isDesktopSearchActive) {
+      isDesktopSearchActive = true;
+      navSearchInput.css({
+        'display': 'block'
+      });
+      navSearchInput.val('');
+      navSearchLabel.css({
+        'display': 'block'
+      });
+    } else {
+      isDesktopSearchActive = false;
+      navSearchInput.css({
+        'display': 'none'
+      });
+      navSearchLabel.css({
+        'display': 'none'
+      });
+    }
+  });
 
   burgerIcon.on('click', function() {
     if(isMobileMenuOpen) {
@@ -41,42 +70,87 @@ $(function() {
     }
   });
 
+  scrollTop.click(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+
   $(window).resize(function() {
     
     hideMobileMenu();
-
     if(!isMobileNav()) {
       removeDisplayNoneSubMenues();
     }
   
     if(window.innerWidth >= 1200) {
-      $("#nav").css({left: 0});
+      $("#nav").css({
+        left: 0,
+        top: 140
+      });
+      $(contactWrapper).css({'display': 'block'});
+    } else {
+      hideContactMenu();
+      $("#nav").css({top: 0});
     }
   });
 
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-    // Do something
 
     console.log(scroll);
-    if(scroll >= 140) {
-      navContainer.css({
-        'position': 'fixed',
-        'top': 0
+    
+
+    if (!isMobileNav()) {
+      if(scroll >= 140) {
+
+        navList.css({
+          'boxShadow': 'none'
+        });
+
+        navWrapper.css({
+          'backgroundColor': '#2A93C9',
+          'boxShadow': '0 0 10px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+        });
+
+        navContainer.css({
+          'position': 'fixed',
+          'top': -20,
+        });
+      } else {
+
+        navList.css({
+          'boxShadow': '0 0 10px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)'
+        });
+
+        navContainer.css({
+          'position': 'absolute',
+          'top': 140
+        });
+
+        navWrapper.css({
+          'backgroundColor': 'transparent',
+          'boxShadow': 'none'
+        });
+      }
+    }
+
+    if(scroll >= 400) {
+      scrollTop.css({
+        'display': 'block'
       });
     } else {
-      navContainer.css({
-        'position': 'absolute',
-        'top': 140
+      scrollTop.css({
+        'display': 'none'
       });
     }
-    
 });
 
   function hideContactMenu() {
     isMobileContactOpen = false;
     $(dotsIcon).removeClass('active');
-    $(contactWrapper).hide();
+    $(contactWrapper).css({
+      'display': 'none'
+    });
   }
 
   function showContactMenu() {
@@ -85,7 +159,9 @@ $(function() {
     }
     isMobileContactOpen = true;
     $(dotsIcon).addClass('active');
-    $(contactWrapper).show();
+    $(contactWrapper).css({
+      'display': 'block',
+    });;
   }
 
   function hideMobileMenu() {
